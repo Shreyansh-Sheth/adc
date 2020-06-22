9777248c385e523d5e8974a964143959732b1ff5
Shreyansh sheth <shethshreyansh777@gmail.com>
2020-06-22 21:25:08 +0530

    after adding firebase support to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './Components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
-function App() {
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-    </div>
-  );
-}
 
-export default App;
+
+
+export default class App extends Component {
+  render() {
+    return (<div>
+      <Navbar></Navbar>
+    </div>);
+
+  }
+
+}
--- src/Services/FirebaseKeys.js
+++ src/Services/FirebaseKeys.js
@@ -1,1 +1,14 @@
-//this page only contains some firebase keys
+//this page only contains some firebase key
+import firebase from 'firebase'
+
+
+let app = firebase.initializeApp({
+    apiKey: '<your-api-key>',
+    authDomain: '<your-auth-domain>',
+    databaseURL: '<your-database-url>',
+    projectId: '<your-cloud-firestore-project>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-sender-id>'
+});
+
+
